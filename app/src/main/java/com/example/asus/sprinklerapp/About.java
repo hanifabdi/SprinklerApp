435ff24f8bbb3132505582caa17cbe03338d80ee
Hanif Abdillah <hanifabdillah61@gmail.com>
2019-04-30 12:30:30 +0700

    Commit Kedua

--- app/src/main/java/com/example/asus/sprinklerapp/About.java
+++ app/src/main/java/com/example/asus/sprinklerapp/About.java
@@ -1,13 +1,25 @@
 package com.example.asus.sprinklerapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class About extends AppCompatActivity {
 
+    private Button btAbout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        btAbout = (Button) findViewById(R.id.btabout);
+        btAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent (About.this, Home.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/asus/sprinklerapp/Home.java
+++ app/src/main/java/com/example/asus/sprinklerapp/Home.java
@@ -1,19 +1,23 @@
 package com.example.asus.sprinklerapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.AppCompatDelegate;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
 public class Home extends AppCompatActivity {
 
     private ViewFlipper vf;
     private Animation fadeIn,fadeOut;
+    ImageView data,history,kalender,tanam,atur,pandu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +31,42 @@ public class Home extends AppCompatActivity {
         vf.setAutoStart(true);
         vf.setFlipInterval(5000);
         vf.startFlipping();
+        tanam = (ImageView) findViewById(R.id.tanam);
+        data = (ImageView) findViewById(R.id.data);
+        history = (ImageView) findViewById(R.id.history);
+        kalender = (ImageView) findViewById(R.id.kalender);
+        atur = (ImageView) findViewById(R.id.atur);
+        pandu = (ImageView) findViewById(R.id.pandu);
+        tanam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent (Home.this, InfoTanaman.class);
+                startActivity(i);
+            }
+        });
+        data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent (Home.this, Data.class);
+                startActivity(i);
+            }
+        });
+
+        atur.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent (Home.this, Pengaturan.class);
+                startActivity(i);
+            }
+        });
+        pandu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent (Home.this, Panduan.class);
+                startActivity(i);
+            }
+        });
+
 
 
     }
@@ -34,32 +74,48 @@ public class Home extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.optionmenu,menu);
-        int nightMode = AppCompatDelegate.getDefaultNightMode();
-        if (nightMode==AppCompatDelegate.MODE_NIGHT_YES){
-            menu.findItem(R.id.night).setTitle("Day Mode");
-        }
-        else {
-            menu.findItem(R.id.night).setTitle("Night Mode");
-        }
         return true;
     }
+    private void showDialog(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                this);
+
+        // set title dialog
+        alertDialogBuilder.setTitle("Keluar dari aplikasi?");
+
+        // set pesan dari dialog
+        alertDialogBuilder
+                .setIcon(R.drawable.alert)
+                .setCancelable(false)
+                .setPositiveButton("Ya",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        // jika tombol diklik, maka akan menutup activity ini
+                        Home.this.finish();
+                    }
+                })
+                .setNegativeButton("Tidak",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // jika tombol ini diklik, akan menutup dialog
+                        // dan tidak terjadi apa2
+                        dialog.cancel();
+                    }
+                });
+
+        // membuat alert dialog dari builder
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // menampilkan alert dialog
+        alertDialog.show();
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId()==R.id.about){
             startActivity(new Intent(this, About.class));
         }
-        else if (item.getItemId() == R.id.night) {
-            int nightMode = AppCompatDelegate.getDefaultNightMode();
-            if (nightMode==AppCompatDelegate.MODE_NIGHT_YES) {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-            } else {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-            }
-            Intent i = new Intent (Home.this, Home.class);
-            startActivity(i);
-        }
+
         else if (item.getItemId() == R.id.out) {
-            startActivity(new Intent(this, Login.class));
+            showDialog();
         }
 
         return true;
